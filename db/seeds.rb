7a8ddca37cabdc51ad84c09d7ff93f272d911b29
Steve Roma <sjroma@hotmail.com>
2017-08-04 15:06:46 -0400

    initial db seeds commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# questionAndAnswer data
+#Question.create!(question: "Who authored 'The Adventures of Tom Sawyer'?", answer1: "Huckleberry Finn", answer2: "John Steinbeck", answer3: "Mark Train", correct4: "Mark Twain")
+#Question.create!(question: "Who authored 'The Grapes of Wrath'?", answer1: "Alexandar Dumas", answer2: "William Shakespere", answer3: "Mark Twain", correct4: "John Steinbeck")
+#Question.create!(question: "Who authored 'Uncle Tom's Cabin?", answer1: "Louisa May Alcott", answer2: "Mary Shelly", answer3: "Mark Twain", correct4: "Harriet Beecher Stowe")
+#Question.create!(question: "Who authored 'Frankenstein'?", answer1: "Lon Chaney", answer2: "Louisa May Alcott", answer3: "Victor Hugo", correct4: "Mary Shelly")
+#Question.create!(question: "Who authored 'The Hitchhikers Guide to the Galaxy'?", answer1: "James T. Kirk", answer2: "Henry Winkler", answer3: "Mork", correct4: "Douglas Adams")
+#Question.create!(question: "Who authored 'The Jungle Books'?", answer1: "Walt Disney", answer2: "Edgar Rice Burroughs", answer3: "Robinson Crusoe", correct4: "Rudyard Kipling")
+#Questions.create!(question: "Who authored 'Moby Dick'?", answer1: "Pee Wee Herman", answer2: "Herman Munster", answer3: "James Fenimore Cooper", correct4: "Herman Melville")
+#Question.create!(question: "Who authored 'Ivanhoe'?", answer1: "Rudyard Kipling", answer2: "Daniel Dafoe", answer3: "Alexander Dumas", correct4: "Walter Scott")
+#Question.create!(question: "Who authored 'Robinson Crusoe'?", answer1: "Willem Dafoe", answer2: "John Steinbeck", answer3: "Mark Twain", correct4: "Daniel Dafoe")
+#Question.create!(question: "Who authored 'Tarzan of the Apes'?", answer1: "Walt Disney", answer2: "", answer3: "", correct4: "Edgar Rice Burroughs")
+
+# quiz names data
+Quiz.create!(name: "Books and Authors")
