ac7c6775e788ed923da1b34facf91fd060a722db
Eric <erickanarr@gmail.com>
2017-08-04 15:01:52 -0400

    table name change to questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170804181607) do
+ActiveRecord::Schema.define(version: 20170804185301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "q_as", force: :cascade do |t|
+  create_table "questions", force: :cascade do |t|
     t.text "question"
     t.string "answer1"
     t.string "answer2"
