cdbeed9abd6b2277251b6bf8e6fbdf0903aa4c99
Eric <erickanarr@gmail.com>
2017-08-08 12:26:17 -0400

    sessions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# admin user data
+User.create!(name: "eric kanarr", admin: "false", email: "eric@gmail.com", password_digest: "password")
+User.create!(name: "ruby gawd", admin: "true", email: "rubygawd@gmail.com", password_digest: "password")
 
 # quiz names data
 Quiz.create!(name: "Books and Authors")
